50d6a8686841ef93e6250565d9fc5dd7a050c990
Judd <keyboardhit21@gmail.com>
2020-03-30 19:40:36 +0800

    added custom form

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -116,8 +116,13 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'users.CustomUser' #custom user model
 
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_UR = 'home'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
